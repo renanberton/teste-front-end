53dc4603e298eb2893af564baf2abce2e5217155
renanberton <renan_berton@yahoo.com.br>
2023-03-25 18:19:29 -0300

    Feature: ScrollToTop também zera o contador de posts agora

--- src/components/Blog/index.tsx
+++ src/components/Blog/index.tsx
@@ -10,51 +10,54 @@ type userType = {
   id: number;
 };
 
+
+
+
 export function Blog() {
-  const [blogs, setblogs] = useState<userType[]>([]);
+  const [posts, setPosts] = useState<userType[]>([]);
   const [dataLoaded, setDataLoaded] = useState<boolean>(false);
   const [openModal, setOpenModal] = useState<boolean>(false);
-  const [openBlog, setopenBlog] = useState<boolean>(true);
+  const [openPost, setOpenPost] = useState<boolean>(true);
   const [idPost, setIdPost] = useState<number>(1);
   const [itemsToShow, setItemsToShow] = useState<number>(25);
-
-  function ScrollToTopButton(){
-    return (
-      <button className='to-top-button' onClick={() => window.scrollTo({ top: 0, behavior: 'smooth' })}>
-        Topo
-      </button>
-    );
-  };
 
   useEffect(() => {
     fetch('https://jsonplaceholder.typicode.com/posts')
       .then((response) => response.json())
       .then((data) => {
-        setblogs(data);
+        setPosts(data);
         setDataLoaded(true);
       });
   }, []);
 
+  function ScrollToTopButton(){
+    return (
+      <button className='to-top-button' onClick={() => {window.scrollTo({ top: 0, behavior: 'smooth' }); setItemsToShow(25)}}>
+        Topo
+      </button>
+    );
+  };
+
   return (
     <>
     <Header />
-      {openModal && <img className='close-icon' src="/imgs/close-icon.png" alt='Imagem para fechar os comentários' width={"50px"} onClick={() => {setOpenModal(false); setopenBlog(true)}} />}
+      {openModal && <img className='close-icon' src="/imgs/close-icon.png" alt='Imagem para fechar os comentários' width={"50px"} onClick={() => {setOpenModal(false); setOpenPost(true)}} />}
       {dataLoaded && openModal && idPost && <ModalBlog id={idPost} />}
-      {openBlog && 
+      {openPost && 
       <div className='container'>
         <h1>Posts</h1>
         <ul className='container-post'>
-          {blogs.slice(0, itemsToShow).map((blog, key) => {
+          {posts.slice(0, itemsToShow).map((post, key) => {
             return (
-              <li className='post' key={key} onClick={() => {setIdPost(blog.id); setOpenModal(true); setopenBlog(false) }}>
-                <h2>{blog.title}</h2>
-                <p>{blog.body}</p>
+              <li className='post' key={key} onClick={() => {setIdPost(post.id); setOpenModal(true); setOpenPost(false) }}>
+                <h2>{post.title}</h2>
+                <p>{post.body}</p>
               </li>
             );
           })}
         </ul>
-        {itemsToShow === blogs.length && <ScrollToTopButton />}
-        {itemsToShow < blogs.length && (
+        {itemsToShow === posts.length && <ScrollToTopButton />}
+        {itemsToShow < posts.length && (
           <button className='load-more' onClick={() => setItemsToShow(itemsToShow + 25)}>Ver Mais</button>
         )}
       </div>
